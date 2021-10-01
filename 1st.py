142418e42a3cec9290b91aa5b5bce50cbee63766
varunbairagi <85159461+varunbairagi@users.noreply.github.com>
2021-10-01 12:08:28 +0530

    Create 1st.py

---
+++ 1st.py
@@ -0,0 +1,9 @@
+''' Program Objective : Write a python program to display a user entered name followed by
+                        Good Afternoon using input() function.
+    Name of Auther : Aniket Tiwari
+    Date of creation : 01-10-2021
+    Date of lastmodification : 01-10-2021
+    Name of the modifing Auther:Aniket Tiwari
+'''
+NAME=str(input("enter the name: "))
+print("Good Afternoon",NAME)
