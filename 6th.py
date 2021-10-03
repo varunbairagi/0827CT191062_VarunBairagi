85dc98ffa6e6838e4c804ade24b94f5f9c0bec63
varunbairagi <85159461+varunbairagi@users.noreply.github.com>
2021-10-03 17:02:23 +0530

    Create 6th.py

---
+++ 6th.py
@@ -0,0 +1,12 @@
+st1 = int(input("enter 1st student's marks : "))
+st2 = int(input("enter 2nd student's marks : "))
+st3 = int(input("enter 3rd student's marks : "))
+st4 = int(input("enter 4th student's marks : "))
+st5 = int(input("enter 5th student's marks : "))
+st6 = int(input("enter 6th student's marks : "))
+
+studentMarksList = [st1, st2, st3, st4, st5, st6]
+
+studentMarksList.sort()
+
+print(studentMarksList)
