33e8396532ffb212d7d9238aeb7d10da639c4b9d
varunbairagi <85159461+varunbairagi@users.noreply.github.com>
2021-10-09 18:55:28 +0530

    Create 10th.py

---
+++ 10th.py
@@ -0,0 +1,14 @@
+myDict = {
+
+    "Jute" : "Shoes", 
+    "mandir" : "Temple",
+    "mitti" : "soil",
+    "takir" : "date",
+    "shabd" : "word",
+}
+
+print("Options are : ", myDict.keys())
+
+a = input("Enter Your Word : ")
+
+print("The meaning of your word is : ", myDict[a])
